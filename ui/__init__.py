a6d1a413aaf69a6619df49e59dcc55fce9226a00
westy4ever <drwessamelguindy@gmail.com>
2025-12-31 12:18:40 +0200

    Delete ui directory

--- ui/__init__.py
+++
@@ -1,6 +0,0 @@
-from .main_screen import PilotFSMain
-from .setup_screen import PilotFSSetup
-from .context_menu import ContextMenuHandler
-from .dialogs import Dialogs
-
-__all__ = ['PilotFSMain', 'PilotFSSetup', 'ContextMenuHandler', 'Dialogs']
