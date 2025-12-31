9ee9db2a497b636dd8ca6358e93b4d600ef1e60f
westy4ever <drwessamelguindy@gmail.com>
2025-12-31 12:18:27 +0200

    Delete network directory

--- network/__init__.py
+++
@@ -1,7 +0,0 @@
-from .remote_manager import RemoteConnectionManager
-from .ftp_client import FTPClient
-from .sftp_client import SFTPClient
-from .webdav_client import WebDAVClient
-from .mount import MountManager
-
-__all__ = ['RemoteConnectionManager', 'FTPClient', 'SFTPClient', 'WebDAVClient', 'MountManager']
